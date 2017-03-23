8b0f37a635e5f4babed649bc56b2678150584bd6
Michael Daffin <michael@daffin.io>
2017-03-23 19:45:51 +0000

    Renames Hello compoent to Controls

--- ui/test/unit/specs/Hello.spec.js
+++ ui/test/unit/specs/Hello.spec.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
-import Hello from '@/components/Hello';
+import Controls from '@/components/Controls';
 
-describe('Hello.vue', () => {
+describe('Controls.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(Hello);
+    const Constructor = Vue.extend(Controls);
     const vm = new Constructor().$mount();
     expect(vm.$el.querySelector('.hello h1').textContent)
       .to.equal('Welcome to Your Vue.js App');
