75eb407df2b5075eaef2087e1ece274a516aab06
mdaffin <michael.daffin@switchconcepts.com>
2017-04-03 16:25:17 +0100

    Removes unnessory dependencies

--- ui/src/main.js
+++ ui/src/main.js
@@ -2,7 +2,6 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import VueResource from 'vue-resource';
-import VueShortKey from 'vue-shortkey';
 import App from './App';
 import router from './router';
 
@@ -11,7 +10,6 @@ import '../node_modules/concise-ui/dist/concise-ui.css';
 
 Vue.config.productionTip = false;
 Vue.use(VueResource);
-Vue.use(VueShortKey);
 Vue.http.options.xhr = { withCredentials: true };
 
 /* eslint-disable no-new */
